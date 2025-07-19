d2fa64a281477b72737a87ee886c3819e063be7d
ankitha2604 <ankithamurthyk2024@gmail.com>
2025-07-19 22:44:02 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,25 @@
+package ankitha;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		byte b = 100;
+		char c = 'a';
+	
+        long l = 200l;
+        short s1 = 300;
+        float f = 400.0f;
+        double d = 500.5d;
+        int i =600;
+        
+        System.out.println(+b);
+        System.out.println(+c);
+       
+        System.out.println(+l);
+        System.out.println(+s1);
+        System.out.println(+f);
+        System.out.println(+d);
+        System.out.println(+i);
+	}
+
+}
---
+++ naturalnum.java
@@ -0,0 +1,16 @@
+package ankitha;
+
+public class naturalnum {
+
+	public static void main(String[] args) {
+		int i;
+		{ System.out.println("natural numbers:");
+		for (i=1;i<=10;i++)
+		   System.out.println(i++);
+		   i=i*1;
+		}
+		
+
+	}
+
+}
