068fdf67141f29c813d26073fc0b36da6aed5b66
ankitha2604 <ankithamurthyk2024@gmail.com>
2025-07-22 15:08:55 +0530

    Add files via upload

--- datatype.java
+++ datatype.java
@@ -1,25 +1,28 @@
-package ankitha;
+package exam;
 
 public class datatype {
-
-	public static void main(String[] args) {
-		byte b = 100;
-		char c = 'a';
-	
-        long l = 200l;
-        short s1 = 300;
-        float f = 400.0f;
-        double d = 500.5d;
-        int i =600;
-        
-        System.out.println(+b);
-        System.out.println(+c);
-       
-        System.out.println(+l);
-        System.out.println(+s1);
-        System.out.println(+f);
-        System.out.println(+d);
-        System.out.println(+i);
+	public static void main (String args[]) {
+		byte b=100;
+		short s=200;
+		long l=300;
+		float f=400.5f;
+		int i=500;
+		double d=600.5d;
+		char c='a';
+		
+		
+		System.out.println(+b);
+		System.out.println(+s);
+		System.out.println(+l);
+		System.out.println(+f);
+		System.out.println(+i);
+		System.out.println(+d);
+		System.out.println(+c);
+		System.out.println();
+		
+		
+		
 	}
+	
 
 }
---
+++ simple.java
@@ -0,0 +1,12 @@
+package exam;
+
+public class simple {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java");
+		System.out.println("java is better than c and c++");
+
+	}
+
+
+}
